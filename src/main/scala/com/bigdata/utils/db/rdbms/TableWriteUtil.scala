7e2a7111bbebf3b2e749190895c254438ebef973
久鸿 <34256397+jiuhonglaugh@users.noreply.github.com>
2018-12-06 18:18:31 +0800

    Update TableWriteUtil.scala

--- src/main/scala/com/bigdata/utils/db/rdbms/TableWriteUtil.scala
+++ src/main/scala/com/bigdata/utils/db/rdbms/TableWriteUtil.scala
@@ -19,28 +19,29 @@ object TableWriteUtil {
     * @param table      表名
     * @return
     */
-  def createTable(rddSchema: StructType, conn: Connection, table: String): Boolean = {
-    Try {
-      val sb = new StringBuilder()
-      rddSchema.fields foreach { field => {
-        val name = field.name
-        val typ: String = field.dataType.toString
-        val nullable = if (field.nullable) "" else "NOT NULL"
-        sb.append(s", $name $typ $nullable")
-      }
-      }
-      if (sb.length < 2) {
-        throw new SQLException(s"Can't find the field information of $table table")
-      }
-      val sql = s"CREATE TABLE $table ($sb.substring(2))"
-      val statement = conn.prepareStatement(sql)
+  def createTable(rddSchema: StructType, conn: Connection, table: String): Unit = {
+    val sb = new StringBuilder()
+    rddSchema.fields foreach { field => {
+      val name = field.name
+      val typ: String = getType(field.dataType)
+      val nullable = if (field.nullable) "" else "NOT NULL"
+      sb.append(s", $name $typ $nullable")
+    }
+    }
+    if (sb.length < 2) {
+    }
+    val sql = s"CREATE TABLE $table (${sb.substring(2)})"
 
-      try {
-        statement.executeQuery()
-      } finally {
-        statement.close()
-      }
-    }.isSuccess
+    val statement = conn.prepareStatement(sql)
+    LOGGER.info(s"$table Table Create SUCCESS")
+    try {
+      statement.execute()
+    } catch {
+      case e: SQLException => LOGGER.error(e.toString); throw new SQLException(s"$table Table Create Failed")
+      case e: Exception => LOGGER.error(e.toString); throw new SQLException(s"$table Table Create Failed")
+    } finally {
+      statement.close()
+    }
   }
 
   /**
@@ -61,24 +62,25 @@ object TableWriteUtil {
     }.isSuccess
   }
 
-  /**
+ /**
     * 删除表
     *
-    * @param conn   数据库连接
-    * @param table  表名
+    * @param conn  数据库链接
+    * @param table 表名
     * @return
     */
-  def dropTable(conn: Connection, table: String): Boolean = {
-    Try {
-      val statement = conn.prepareStatement(s"DROP TABLE $table")
-      try {
-        statement.executeUpdate()
-      } finally {
-        statement.close()
-      }
-    }.isSuccess
+  def dropTable(conn: Connection, table: String): Unit = {
+    val statement = conn.prepareStatement(s"DROP TABLE $table")
+    try {
+      statement.executeUpdate()
+      LOGGER.warn(s"$table Table Drop SUCCESS")
+    } catch {
+      case e: SQLException => LOGGER.error(e.toString); throw new SQLException(s"$table Table Drop Failed")
+      case e: Exception => LOGGER.error(e.toString); throw new SQLException(s"$table Table Drop Failed")
+    } finally {
+      statement.close()
+    }
   }
-
   /**
     * 清空表
     *
@@ -123,6 +125,31 @@ object TableWriteUtil {
     val placeholders = rddSchema.fields.map(_ => "?").mkString(",")
     val sql = s"REPLACE INTO $table ($columns) VALUES ($placeholders)"
     conn.prepareStatement(sql)
+  }
+ 
+ /**
+    * 根据数据类型进行模式匹配
+    *
+    * @param fileType 字段数据类型
+    * @return
+    */
+  private def getType(fileType: DataType): String = {
+    fileType match {
+      case IntegerType => "int(10)"
+      case LongType => "BigInt(20)"
+      case DoubleType => "double(10,2)"
+      case FloatType => "float(10,2)"
+      case ShortType => "year"
+      case ByteType => "blob"
+      case BooleanType => "boolean"
+      case StringType => "text"
+      case BinaryType => "binary(3)"
+      case TimestampType => "datetime"
+      case DateType => "date"
+      case _: DecimalType => "decimal(10,2)"
+      case _ => throw new IllegalArgumentException(
+        s"Can't translate non-null value for field $fileType")
+    }
   }
 
 }
